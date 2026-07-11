63bc7abcb88cdccf71087b9d04e902e79397f9ff
warrior-0 <bodla0330@gmail.com>
2026-07-11 16:55:49 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = https://warrior-0.github.io;
+const API_BASE_URL = "https://warrior-0.github.io";
 
 async function apiRequest(path, options = {}) {
     const response = await fetch(`${API_BASE_URL}${path}`, {
