186030fbca9f719c907c9cd72c1b98e225d203b3
warrior-0 <bodla0330@gmail.com>
2026-08-03 00:59:57 +0900

    Update log message for augment activation condition

--- backend/game.js
+++ backend/game.js
@@ -494,7 +494,7 @@ function processAugmentQueue(game, event) {
         if (fireAugment(game, entry, event)) {
             toRemoveIndices.push(index);
         } else {
-            game.log.push(`- ${entry.augment.name}: 발동 조건 미충족으로 다음 증강을 검사합니다.`);
+            game.log.push(`- ${entry.augment.name}: 발동 조건이 맞지 않아 다음 증강을 검사합니다.`);
         }
     }
 
